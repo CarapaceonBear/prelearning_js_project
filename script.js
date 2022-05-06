cc3b2cd9ce668960bbc3537239a3f73e1d6366b4
carapaceonbear <seerhan17@gmail.com>
2022-05-06 15:01:54 +0100

    number 2 complete: days to age

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function checkPolarity(event) {
     let polarityNumber = event.target.value;
     if (polarityNumber >= 0) {
-        alert("That number is positive")
+        alert("That number is positive");
     } else {
-        alert("That number is negative")
+        alert("That number is negative");
     }
 }
 
 function daysToAge(event){
-    pass;
+    let days = event.target.value;
+    let years = Math.floor(days / 365);
+    let remainder = (days - (years * 365))
+
+    if (years == 1) {
+        alert ("You would be " + years + " year old, and " + remainder + " days");
+    } else {
+        alert ("You would be " + years + " years old, and " + remainder + " days");
+    }
 }
 
 function largestOfThree(event){
