ac6acd44657714ce09ffa130b2a22aba26731211
carapaceonbear <seerhan17@gmail.com>
2022-05-06 14:41:42 +0100

    initial commit, empty functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function checkPolarity(event) {
+    pass;
+}
+
+function daysToAge(event){
+    pass;
+}
+
+function largestOfThree(event){
+    pass;
+}
+
+function lastName(event){
+    pass;
+}
+
+function allNumbersPositive(event){
+    pass;
+}
